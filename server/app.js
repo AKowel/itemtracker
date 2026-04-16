956745bc6b987ac69618901f1b4e29abe6620b7e
AKowel <aksels.o@proton.me>
2026-04-16 17:33:30 +0100

    HeatmapUpdate v13

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ async function createApp() {
   app.set("views", path.join(process.cwd(), "views"));
 
   app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+  app.use(express.json({ limit: '50mb' }));
   app.use(
     session({
       name: "itemtracker.sid",
